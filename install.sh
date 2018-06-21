0a9cf0f89b3e251c039f3ab1d5488aebaf8cab30
Naoki Kinoshita <uttnaoki@gmail.com>
2018-06-21 19:53:12 +0900

    clean install.sh

--- install.sh
+++ install.sh
@@ -1,23 +1,23 @@
 #!/bin/sh
 
-# ターミナル(OS)の種類を判断
+# OS(ターミナル)の種類を判断
 if [ "$(uname)" = 'Darwin' ]; then
   OS='Mac'
 elif [ $(echo $(uname -r) | grep -e 'Microsoft') ]; then
   OS='WSL'
-elif [ "$(expr substr $(uname -s) 1 5)" = 'Linux' ]; then
-  OS='Linux'
-elif [ "$(expr substr $(uname -s) 1 10)" = 'MINGW32_NT' ]; then
-  OS='Cygwin'
+# elif [ "$(expr substr $(uname -s) 1 5)" = 'Linux' ]; then
+#   OS='Linux'
+# elif [ "$(expr substr $(uname -s) 1 10)" = 'MINGW32_NT' ]; then
+#   OS='Cygwin'
 else
   echo "Your platform ($(uname -a)) is not supported."
   OS='undefined'
 fi
 echo $OS' terminal\n'
 
-# Mac の場合，Mac ディレクトリを参照
-if [ $OS = "Mac" -o $OS = "WSL" ]; then
-  for f in $OS/.??*
+# ホームディレクトリに dotfile のリンクを貼る．
+function link_dotfile_to_home() {
+  for f in $1/.??*
   do
     # ファイルの絶対パスを取得
     dotfile_path=$HOME/dotfiles/$f
@@ -31,34 +31,19 @@ if [ $OS = "Mac" -o $OS = "WSL" ]; then
 
     # ファイルのリンクが既に貼られていれば skip
     if [ -h $HOME/$dotfile_name ]; then
-      echo "[skip] "$OS"/"$dotfile_name
-    # ファイルのリンクが既に貼られていなければ，シンボリックリンクを貼る
+      echo "[skip] "$dotfile_name
+      # ファイルのリンクが既に貼られていなければ，シンボリックリンクを貼る．
     else
       ln -s $dotfile_path $HOME/$dotfile_name
-      echo "[OK] "$OS"/"$dotfile_name
+      echo "[OK] "$dotfile_name
     fi
   done
+}
+
+# 各OS(ターミナル)に応じて dotfile のリンクを貼る．
+if [ ! $OS = "undefined" ]; then
+  link_dotfile_to_home $OS
 fi
 
-# 各dotfileにリンクを貼る
-for f in .??*
-do
-  # ファイルの絶対パスを取得
-  dotfile_path=$HOME/dotfiles/$f
-  # ファイルパスの一番右の"/"以降の文字列を取得
-  dotfile_name=${dotfile_path##*/}
-
-  # 特定のファイルは pass
-  [ "$dotfile_name" = ".git" ] && continue
-  [ "$dotfile_name" = ".gitignore" ] && continue
-  [ "$dotfile_name" = ".DS_Store" ] && continue
-
-  # ファイルのリンクが既に貼られていれば skip
-  if [ -h $HOME/$dotfile_name ]; then
-    echo "[skip] "$dotfile_name
-  # ファイルのリンクが既に貼られていなければ，シンボリックリンクを貼る
-  else
-    ln -s $dotfile_path $HOME/$dotfile_name
-    echo "[OK] "$dotfile_name
-  fi
-done
+# 各OS(ターミナル)共通の dotfile のリンクを貼る．
+link_dotfile_to_home "."
